ac959e6fc0faa28395cbed19a463ea46737b48b4
Bob M <BobMcHenryVideo@gmail.com>
2019-02-26 20:12:35 -0800

    Adding post method and addfilm form

--- db.js
+++ db.js
@@ -2,4 +2,5 @@ var db = require('mysql');
 var dbCred = require('./db_creds.json');
 var connection = db.createConnection(dbCred);
 
+
 module.exports = connection;
--- routes/film.js
+++ routes/film.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
+
 router.get('/all', function(req, res, next) {
   db.query("select * from film", (error, result, fields) => {
     if (error) {
@@ -30,13 +31,34 @@ router.get('/rating/:rating', function(req, res, next) {
   });
 });
 
+
+
+router.post('/addFilm', function(req,res,next){
+  var query = "INSERT INTO film (title, description, release_year, language_id,"
+    +" rental_duration, rental_rate, length, replacement_cost, rating) "
+    +"VALUES ( ?, ?, ?, ?, 3, 4.99, ?, 19.99, 'G');"
+  var queryParams = [
+    req.body.title,
+    req.body.description,
+    parseInt(req.body.release_year),
+    parseInt(req.body.language_id),
+    parseInt(req.body.length)
+  ];
+
+  db.query(query, queryParams, (error, result, fields) => {
+    if (error){
+      res.status(500).send(error);
+    }
+    res.status(201).send('record added');
+  });
+});
+
 router.get('/:id', function(req, res, next) {
   var sqlQuery = "select * from film where film_id=?";
   db.query(sqlQuery, [req.params.id], (error, result, fields) => {
     if (error) {
       res.status(500).send(error);
     }
-    console.log(sqlQuery);
     res.send(result);
   });
 });
